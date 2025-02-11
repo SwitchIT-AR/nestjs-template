19635d1277c257400d0e97c53140f31feafc39d9
ext4cats <ext4cats@farmarrays.com>
2025-02-11 11:00:00 -0300

    set up mikro-orm module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,24 @@
+import { MikroOrmModule } from '@mikro-orm/nestjs';
+import { PostgreSqlDriver } from '@mikro-orm/postgresql';
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [ConfigModule.forRoot({ cache: true })],
+  imports: [
+    ConfigModule.forRoot({ cache: true }),
+    MikroOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      driver: PostgreSqlDriver,
+      useFactory: (configService: ConfigService) => ({
+        driver: PostgreSqlDriver,
+        entities: ['./dist/**/*.entity.js'],
+        entitiesTs: ['./src/**/*.entity.ts'],
+        ignoreUndefinedInQuery: true,
+        debug: configService.get('NODE_ENV') !== 'production',
+        clientUrl: configService.getOrThrow('DATABASE_URL'),
+      }),
+    }),
+  ],
 })
 export class AppModule {}
