2fcb2973737c09b5e43b051b25891b2aa24e0eae
ext4cats <ext4cats@farmarrays.com>
2025-02-11 10:40:32 -0300

    add global api prefix

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { ZodFilter } from './zod.filter';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.disable('x-powered-by');
+  app.setGlobalPrefix('api');
   app.useGlobalFilters(new ZodFilter());
   await app.listen(8080);
 }
