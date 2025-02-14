8d4055082d8b0031165ac59a2064597191f5f520
ext4cats <ext4cats@farmarrays.com>
2025-02-14 11:58:06 -0300

    add session entity

---
+++ src/auth/domain/session.entity.ts
@@ -0,0 +1,33 @@
+import {
+  Entity,
+  ManyToOne,
+  PrimaryKey,
+  Property,
+  ref,
+  Ref,
+} from '@mikro-orm/core';
+import * as uuid from 'uuid';
+import { TimestampedEntity } from '../../common/timestamped-entity.entity';
+import { User } from '../../users/domain/user.entity';
+
+@Entity({ tableName: 'sessions' })
+export class Session extends TimestampedEntity {
+  @PrimaryKey({ type: 'uuid' })
+  readonly id: string = uuid.v7();
+
+  @ManyToOne(() => User, { ref: true })
+  readonly user: Ref<User>;
+
+  @Property({ type: 'timestamptz' })
+  readonly expireAt: Date;
+
+  constructor(props: SessionProps) {
+    super();
+    this.user = ref(props.user);
+    this.expireAt = new Date(Date.now() + 8 * 3600 * 1000);
+  }
+}
+
+interface SessionProps {
+  readonly user: User;
+}
