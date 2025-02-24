f98e7e7d36326e0e694dd65e184c6ab2f51289cf
ext4cats <ext4cats@farmarrays.com>
2025-02-24 12:16:05 -0300

    add missing import in auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,13 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import cookieParser from 'cookie-parser';
+import { UsersModule } from '../users/users.module';
 import { AuthController } from './auth.controller';
 import { AuthGuardProvider } from './auth.guard';
 import { ConfigurableModuleClass } from './auth.module-definition';
 import { AuthService } from './auth.service';
 
 @Module({
+  imports: [UsersModule],
   providers: [AuthService, AuthGuardProvider],
   controllers: [AuthController],
 })
