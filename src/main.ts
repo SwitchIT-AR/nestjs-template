451811f76e9b224d203a5a7a565ae05f9534b035
ext4cats <ext4cats@farmarrays.com>
2025-05-15 18:51:59 -0300

    add support for changing port

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
@@ -6,7 +7,9 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.disable('x-powered-by');
   app.setGlobalPrefix('api');
-  await app.listen(8080);
+  const configService = app.get(ConfigService);
+  const port = configService.get<string>('PORT') ?? '8080';
+  await app.listen(port);
 }
 
 void bootstrap();
