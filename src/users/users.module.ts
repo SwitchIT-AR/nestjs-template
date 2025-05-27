9a8e62c846bfcb2a5232e0823f22798fae973dfc
ext4cats <ext4cats@farmarrays.com>
2025-05-27 13:01:14 -0300

    add users controller

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PasswordService } from './password.service';
+import { UsersController } from './users.controller';
 import { UsersRepository } from './users.repository';
 import { UsersService } from './users.service';
 
 @Module({
+  controllers: [UsersController],
   providers: [PasswordService, UsersRepository, UsersService],
   exports: [PasswordService, UsersRepository],
 })
