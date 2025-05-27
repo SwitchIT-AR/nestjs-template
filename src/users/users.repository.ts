1ee94647e4ba7f848e00baa0fbc5d7e11c05b857
ext4cats <ext4cats@farmarrays.com>
2025-05-27 11:17:03 -0300

    add findOneById method to users repository

--- src/users/users.repository.ts
+++ src/users/users.repository.ts
@@ -1,4 +1,12 @@
+import { EntityManager } from '@mikro-orm/postgresql';
 import { Injectable } from '@nestjs/common';
+import { User } from './entities/user.entity';
 
 @Injectable()
-export class UsersRepository {}
+export class UsersRepository {
+  constructor(private readonly em: EntityManager) {}
+
+  async findOneById(id: string) {
+    return this.em.findOne(User, { id });
+  }
+}
