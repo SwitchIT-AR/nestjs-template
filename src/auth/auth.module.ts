4e463716cc072ca200effc0e23094a435679a79f
ext4cats <ext4cats@farmarrays.com>
2025-05-27 13:43:49 -0300

    add auth controller

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,5 +1,6 @@
 import { UsersModule } from '@/users';
 import { Module } from '@nestjs/common';
+import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import {
   AuthenticationGuard,
@@ -13,6 +14,7 @@ import { SessionsRepository } from './sessions.repository';
 
 @Module({
   imports: [UsersModule],
+  controllers: [AuthController],
   providers: [
     SessionsRepository,
     AuthService,
