f59b7ff2c5eec141330d4da0bbd8b99b9b40ce36
ext4cats <ext4cats@farmarrays.com>
2025-05-27 14:10:09 -0300

    add missing config module import to users module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { PasswordService } from './password.service';
 import { UsersController } from './users.controller';
 import { UsersRepository } from './users.repository';
 import { UsersService } from './users.service';
 
 @Module({
+  imports: [ConfigModule],
   controllers: [UsersController],
   providers: [PasswordService, UsersRepository, UsersService],
   exports: [PasswordService, UsersRepository],
