192eeddb89e743a208daf9d23eeb2033222b6ad8
ext4cats <ext4cats@farmarrays.com>
2025-05-27 12:47:56 -0300

    add role enum to users module exports

--- src/users/index.ts
+++ src/users/index.ts
@@ -1,4 +1,5 @@
 export { MAX_PASSWORD_LENGTH } from './constants';
+export { Role } from './entities/role.enum';
 export { User } from './entities/user.entity';
 export { PasswordService } from './password.service';
 export { UsersModule } from './users.module';
