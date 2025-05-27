3f17ba0ef3de8e106494f171a09e5113caccc8bb
ext4cats <ext4cats@farmarrays.com>
2025-05-27 11:14:13 -0300

    implement create user method

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import {
+  EntityManager,
+  UniqueConstraintViolationException,
+} from '@mikro-orm/postgresql';
+import { ConflictException, Injectable } from '@nestjs/common';
+import { User } from './entities/user.entity';
+import { PasswordService } from './password.service';
+import { NewUser } from './schemas/new-user.schema';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    private readonly passwordService: PasswordService,
+    private readonly em: EntityManager,
+  ) {}
+
+  async createUser(data: NewUser) {
+    const { password, ...rest } = data;
+    const passwordHash = await this.passwordService.hash(password);
+    const user = new User({ passwordHash, ...rest });
+    try {
+      await this.em.persist(user).flush();
+    } catch (err: unknown) {
+      if (err instanceof UniqueConstraintViolationException)
+        throw new ConflictException('Username is already in use');
+      throw err;
+    }
+  }
+}
