5a5b02819ad4d97e83fe38d28e77361d307c2f56
ext4cats <ext4cats@farmarrays.com>
2025-05-27 13:13:27 -0300

    update zod pipe to use v4 ZodType

--- src/common/zod.pipe.ts
+++ src/common/zod.pipe.ts
@@ -1,9 +1,9 @@
 import { Injectable, PipeTransform } from '@nestjs/common';
-import { ZodTypeAny } from 'zod';
+import { ZodType } from 'zod/v4';
 
 @Injectable()
 export class ZodPipe implements PipeTransform {
-  constructor(private readonly schema: ZodTypeAny) {}
+  constructor(private readonly schema: ZodType) {}
 
   transform(value: unknown) {
     return this.schema.parseAsync(value);
