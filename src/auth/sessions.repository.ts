0fe1732c63798b9cef56ff3114902c003897d999
ext4cats <ext4cats@farmarrays.com>
2025-05-27 12:41:36 -0300

    add findOneById method to sessions repository

--- src/auth/sessions.repository.ts
+++ src/auth/sessions.repository.ts
@@ -1,4 +1,12 @@
+import { EntityManager } from '@mikro-orm/postgresql';
 import { Injectable } from '@nestjs/common';
+import { Session } from './entities/session.entity';
 
 @Injectable()
-export class SessionsRepository {}
+export class SessionsRepository {
+  constructor(private readonly em: EntityManager) {}
+
+  async findOneById(id: string) {
+    return this.em.findOne(Session, { id });
+  }
+}
