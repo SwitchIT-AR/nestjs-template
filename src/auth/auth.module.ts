f06808e8f49baf78033f3b2960cacd74d630ab31
ext4cats <ext4cats@farmarrays.com>
2025-05-27 12:09:11 -0300

    add auth service to auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
+import { AuthService } from './auth.service';
 import { SessionsRepository } from './sessions.repository';
 
 @Module({
-  providers: [SessionsRepository],
+  providers: [SessionsRepository, AuthService],
 })
 export class AuthModule {}
