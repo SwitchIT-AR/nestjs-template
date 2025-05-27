2fcb3a6a76664a98ac3aef36f8bdd9d676287c1d
ext4cats <ext4cats@farmarrays.com>
2025-05-27 13:10:02 -0300

    add decorators to auth module exports

--- src/auth/index.ts
+++ src/auth/index.ts
@@ -1,1 +1,6 @@
 export { AuthModule } from './auth.module';
+export { CurrentSession } from './decorators/current-session.decorator';
+export { RequirePermission } from './decorators/require-permission.decorator';
+export { SkipAuthentication } from './decorators/skip-authentication.decorator';
+export { SkipAuthorization } from './decorators/skip-authorization.decorator';
+export { Session } from './entities/session.entity';
